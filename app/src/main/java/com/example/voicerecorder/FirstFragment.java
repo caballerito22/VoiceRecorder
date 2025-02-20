8533c6e8cddf2a9afa77812318f68e37f2781732
caballerito22 <alecabnun@alu.edu.gva.es>
2025-02-20 17:19:03 +0100

    Initial commit

---
+++ app/src/main/java/com/example/voicerecorder/FirstFragment.java
@@ -0,0 +1,188 @@
+package com.example.voicerecorder;
+
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.Manifest;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.voicerecorder.databinding.FragmentFirstBinding;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FirstFragment extends Fragment {
+
+    private FragmentFirstBinding binding;
+
+    private MediaRecorder mediaRecorder;
+    private MediaRecorder mRecorder=null;
+    private MediaPlayer mPlayer=null;
+
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+    private boolean permissionToRecordAccepted = false;
+    private String [] permissions = {Manifest.permission.RECORD_AUDIO};
+
+    private ImageView logo;
+    private FloatingActionButton record;
+    private FloatingActionButton stop;
+    private FloatingActionButton play;
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        //return binding.getRoot();
+        ActivityCompat.requestPermissions(getActivity(), permissions,
+                REQUEST_RECORD_AUDIO_PERMISSION);
+
+        //creo una instancia
+        mediaRecorder = new MediaRecorder();
+        //ESTABLECEMOS LA FUENTE USANDO MIC
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        //ESTABLECEMOS EL FORMATO DEL FICHERO DE SALIDA
+        mediaRecorder.setOutputFormat (MediaRecorder.OutputFormat.MPEG_2_TS);
+
+
+        //establecemos el codificador
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+
+        return binding.getRoot();
+    }
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode,
+            String[] permissions,
+            int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_RECORD_AUDIO_PERMISSION:
+                permissionToRecordAccepted = grantResults[0] ==
+                        PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+        if (!permissionToRecordAccepted) {
+            Toast.makeText(
+                    getContext(),
+                    "Permission needed",
+                    Toast.LENGTH_LONG
+            ).show();
+            getActivity().finish();
+        }
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //asignamos el resultado
+        logo = view.findViewById(R.id.logo);
+        record = view.findViewById(R.id.record);
+        stop = view.findViewById(R.id.stop);
+        play = view.findViewById(R.id.play);
+
+
+        //definimos los listeners
+        record.setOnClickListener(v -> {
+            logo.setImageDrawable(
+                    getResources().getDrawable(R.drawable.recording)
+    );
+        });
+
+        stop.setOnClickListener(v -> {
+            logo.setImageDrawable(
+                    getResources().getDrawable(R.drawable.stop)
+    );
+        });
+
+        play.setOnClickListener(v -> {
+            logo.setImageDrawable
+                    (getResources().getDrawable(R.drawable.playing)
+            );
+        });
+
+        //definimos nombre y ruta
+        String ruta = getContext().getExternalFilesDir(null).getAbsolutePath();
+        var fileName = ruta + "/audiorecord.3gp";
+
+        //iniciamos la grabación
+        record.setOnClickListener(v -> {
+            logo.setImageDrawable(
+                    getResources().getDrawable(R.drawable.playing));
+            if (mRecorder == null) {
+                mRecorder = new MediaRecorder();
+                mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+                mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+                mRecorder.setOutputFile(fileName);
+                mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+                try {
+                    //iniciamos
+                    mRecorder.prepare();
+                } catch (IOException e) {
+                    Log.e("RECORDING",
+                            "No se puede iniciar la grabación bbito");
+                }
+
+                mRecorder.start();
+            }
+        });
+
+        play.setOnClickListener(v -> {
+            logo.setImageDrawable
+                    (getResources().getDrawable(R.drawable.playing));
+            if (mRecorder == null && mPlayer == null) {
+                mPlayer = new MediaPlayer();
+
+                try {
+                    mPlayer.setDataSource(fileName);
+                    mPlayer.prepare();
+                    mPlayer.start();
+
+                    mPlayer.setOnCompletionListener(mediaPlayer -> {
+                        stop.callOnClick();
+                    });
+                } catch (IOException e) {
+                    Log.e("RECORDING", "No es pot iniciar la reproducció");
+                }
+            }
+        });
+
+        stop.setOnClickListener(v -> {
+            logo.setImageDrawable(
+                    getResources().getDrawable(R.drawable.stop));
+
+            if (mRecorder != null) {
+                mRecorder.stop();
+                mRecorder.reset();
+                mRecorder.release();
+                mRecorder = null;
+            } else if (mPlayer != null) {
+                mPlayer.stop();
+                mPlayer = null;
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
